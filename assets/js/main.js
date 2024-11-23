0208ac85bd5512a4c98cbda1f5a69212f00ca50a
New Username <new.email@example.com>
2024-11-23 09:29:39 +0400

    BA

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const accordionHeaders = document.querySelectorAll(".accordion-header");
-  
-    accordionHeaders.forEach(header => {
-      header.addEventListener("click", () => {
-        const content = header.nextElementSibling;
-  
-        document.querySelectorAll(".accordion-content").forEach(item => {
-          if (item !== content && item.classList.contains("open")) {
-            item.classList.remove("open");
-          }
-        });
-  
-        content.classList.toggle("open");
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
+  const loginPopup = document.getElementById("popupParent");
+  const loginBtn = document.getElementById("loginBtn");
+  const popupCloseBtn = document.getElementById("popupCloseBtn");
+
+  accordionHeaders.forEach((header) => {
+    header.addEventListener("click", () => {
+      const content = header.nextElementSibling;
+
+      document.querySelectorAll(".accordion-content").forEach((item) => {
+        if (item !== content && item.classList.contains("open")) {
+          item.classList.remove("open");
+        }
       });
+
+      content.classList.toggle("open");
     });
-
-
-    const loginBtn = document.getElementById("loginBtn");
-
-      loginBtn.addEventListener("click", () => {
-  
-        const loginPopup = document.getElementById("popupParent");
-        const body = document.getElementById('body');
-        window.scrollTo(0, 0);
-
-        loginPopup.classList.toggle('d-none');
-        body.classList.toggle('overflow-hidden')
-        
-
-      });
   });
+
+  loginBtn.addEventListener("click", () => {
+    loginPopup.classList.toggle("d-none");
+  });
+
+  popupCloseBtn.addEventListener("click", () => {
+    loginPopup.classList.toggle("d-none");
+  });
+});
