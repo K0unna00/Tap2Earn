b7100fd72e9396df568b60765bdbb0ed83935cff
K0unna00 <78pf4jz@code.edu.az>
2024-11-25 12:29:24 +0400

    BA

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,7 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const tabProfileBtn = document.getElementById("tab-profile");
     const profileForm = document.getElementById("profileForm");
     const withdrawSection = document.getElementById("withdrawSection");
-
+    const selected = document.querySelector('.selected-option');
+    const options = document.querySelector('.dropdown-options');
+    const addAddressBtn = document.getElementById('addAddressBtn');
+    const confirmAddressBtn = document.getElementById('confirmAddressBtn');
+    const addressPopup = document.getElementById('addressPopupParent');
+    const addressSuccessPopup = document.getElementById('addressSuccessPopupParent');
+    const addressPopupCloseBtn = document.getElementById('addressPopupCloseBtn');
+    const addressSuccesssPopupCloseBtn = document.getElementById('addressSuccesssPopupCloseBtn');
 
     Array.from(tabBtns).forEach(tab => {
         tab.addEventListener("click", function () {
@@ -22,15 +29,9 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-
-
-    const selected = document.querySelector('.selected-option');
-    const options = document.querySelector('.dropdown-options');
-
     selected.addEventListener('click', () => {
         options.style.display = options.style.display === 'block' ? 'none' : 'block';
     });
-
     document.querySelectorAll('.dropdown-options li').forEach((item) => {
         item.addEventListener('click', () => {
             selected.innerHTML = item.innerHTML;
@@ -38,7 +39,20 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-  
 
+    addAddressBtn.addEventListener("click", () => {
+        addressPopup.classList.toggle("d-none");
+    });
+    addressPopupCloseBtn.addEventListener("click", () => {
+        addressPopup.classList.toggle("d-none");
+    });
+
+    confirmAddressBtn.addEventListener("click", () => {
+        addressPopup.classList.toggle("d-none");
+        addressSuccessPopup.classList.toggle('d-none');
+    });
+    addressSuccesssPopupCloseBtn.addEventListener("click", () => {
+        addressSuccessPopup.classList.toggle("d-none");
+    });
 });
 
